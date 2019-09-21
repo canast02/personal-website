cf1703fa5e6afd5d237920c04ead9041266c92f8
Chrysovalantis Anastasiou <chrysovalantis.anastasiou@gmail.com>
2019-09-21 16:57:13 -0700

    deployment

---
+++ deploy/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd _site
+    git init
+
+    git remote add deploy "deploy@canastas.info:/var/www/canastas.info"
+    git config user.name "Travis CI"
+    git config user.email "chrysovalantis.anastasiou+travisci@gmail.com"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
