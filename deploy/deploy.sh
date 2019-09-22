eaf24800b20d2b1f28a35b4dc4d30257c8333f23
Chrysovalantis Anastasiou <chrysovalantis.anastasiou@gmail.com>
2019-09-21 17:13:30 -0700

    updated README

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,18 +2,18 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
     cd _site
-    git init
-
-    git remote add deploy "deploy@canastas.info:/var/www/canastas.info"
-    git config user.name "Travis CI"
-    git config user.email "chrysovalantis.anastasiou+travisci@gmail.com"
 
     # Remove unnecessary files.
     rm deploy-key.enc
     rm README.md
     rm -rf deploy
+
+    # Initialize a new git repo in _site, and push it to our server.
+    git init
+    git remote add deploy "deploy@canastas.info:/var/www/canastas.info"
+    git config user.name "Travis CI"
+    git config user.email "chrysovalantis.anastasiou+travisci@gmail.com"
 
     git add .
     git commit -m "Deploy"
